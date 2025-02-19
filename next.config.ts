0e3fa2d95edb6181cf22c40f5f2a69b752948f15
Chakradhar-Malage <chessp526@gmail.com>
2025-02-19 13:17:52 +0530

    added .next folder on github

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,17 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
-
 // import type { NextConfig } from "next";
 
 // const nextConfig: NextConfig = {
-//   basePath: '/webbuild',
-//   // ...other config options if needed
+//   /* config options here */
 // };
 
 // export default nextConfig;
+
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  basePath: '/webbuild',
+  // ...other config options if needed
+};
+
+export default nextConfig;
 
